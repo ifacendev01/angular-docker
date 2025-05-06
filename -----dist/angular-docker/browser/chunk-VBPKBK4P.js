75de7c5ed2d529006b2ef7afb95392ffd8e91661
Mario Murillo <murillo@ifacus.com>
2025-05-06 16:45:47 -0500

    Send Docker Hub

--- -----dist/angular-docker/browser/chunk-VBPKBK4P.js
+++
@@ -1,1 +0,0 @@
-var e="$",t=new Date().getFullYear(),r={name:"Techzaa",buyLink:""},c="/";export{e as a,t as b,r as c,c as d};
--- -----dist/angular-docker/browser/chunk-WEUHPWUH.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-WFD4CTXP.js";import"./chunk-WWX6BADO.js";export{a as default};
